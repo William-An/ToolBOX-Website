a6bb5f999f5134614ae4717a1bf773580101f02c
Weili An <china_aisa@live.com>
2017-05-07 03:58:39 +0000

    Js load HTML

---
+++ loadHtml.php
@@ -0,0 +1,4 @@
+<?php
+    $tmp = file_get_contents("./static/Template/" . $_GET["tmp"] . ".html")
+    
+?>
--- static/js/own.js
+++ static/js/own.js
@@ -1,21 +1,25 @@
-function selectButton(it){
-    var nodelist = it.parentElement.parentElement.children; // Get button list
+function selectButton(element){
+    var nodelist = element.parentElement.parentElement.children; // Get button list
     for(var i = 0; i < nodelist.length; i++){
         nodelist[i].children.item(0).setAttribute("class","button");
     }
-    it.setAttribute("class","button special");
-    var citationform = document.getElementById("citationform"); // Get citationform
-    switch (it.textContent) {   // Separate this from selectButton, allow it to be a function
-        case 'MLA': // Switch to using template
-            citationform.innerHTML="<h2>CitationBox</h2>";
-            break;
-        case 'APA':
-            citationform.innerHTML="<h2>CitationBox</h2>";
-            break;
-        case 'Wiki':
-            citationform.innerHTML="<h2>CitationBox</h2>";
-            break;
-        default:
-            // code
+    element.setAttribute("class","button special");
+}
+function loadTemplate(element,id){
+    var tmp = document.querySelector('#'+id);
+    var par = element.parentElement.parentElement.parentElement.parentElement;
+    try{
+        // console.log(par.contains(tmp.content.cloneNode(true))); // Always return false because (tmp.content.cloneNode(true) hasn't loaded in DOM
+        var lis = tmp.content.cloneNode(true).children;
+        for(var i =0; i < lis.length; i++){
+            par.removeChild(par.querySelector(lis[i].tagName));
+        }
+        par.appendChild(tmp.content.cloneNode(true));
+        
     }
+    catch(err){
+        console.log(err);
+        par.appendChild(tmp.content.cloneNode(true));
+    }
+    console.log(par.childNodes)
 }
